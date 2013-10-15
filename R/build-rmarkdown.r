bb4f3c52f304f01196f0b85d2dd923486eecb11f
Jeromy <jkanglim@gmail.com>
2013-10-15 14:41:33 +1100

    Tried to add some sample vignettes
    not compiling with R CMD INSTALL at the moment

---
+++ R/build-rmarkdown.r
@@ -0,0 +1,7 @@
+###
+# library(knitr)
+# knit(input='vignettes/bootcorrelations-intro.Rmd', output='inst/doc/bootcorrelations-intro.md')
+# library(markdown)
+# markdownToHTML('inst/doc/bootcorrelations-intro.md', 'inst/doc/bootcorrelations-intro.html')
+
+NULL
